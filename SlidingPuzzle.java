012891349c3ba9f5780efed093d97558eb948d23
Sumedha Singh Rathor <sumedharathor@gmail.com>
2025-03-22 01:31:31 +0530

    Proper Identation

--- SlidingPuzzle.java
+++ SlidingPuzzle.java
@@ -5,25 +5,25 @@ import java.util.*;
 class SlidingPuzzle extends Frame implements ActionListener, KeyListener {
     int i, j, h, v, move, c;
     
-    String msg="Number of Attempts is";
+    String msg = "Number of Attempts is";
     
-    Button b[][]=new Button[4][4];
+    Button b[][] = new Button[4][4];
 
-    Label l1=new Label("Number Puzzle",Label.CENTER);
-    Label l2=new Label(msg,Label.CENTER);
+    Label l1 = new Label("Number Puzzle",Label.CENTER);
+    Label l2 = new Label(msg,Label.CENTER);
     
     Font f1 = new Font("SansSerif", Font.BOLD, 18);
     Font f2 = new Font("SansSerif", Font.BOLD, 16);
     Font f3 = new Font("SansSerif", Font.BOLD, 26);
 
-    int[] solutionArray = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,13,14,15 };
+    int[] solutionArray = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 
-    Panel p=new Panel();
+    Panel p = new Panel();
 
-    public Num(String s) {
+    public SlidingPuzzle(String s) {
         super(s);
         setLayout(new BorderLayout());
-        move=0;
+        move = 0;
         shuffleArray(solutionArray);
         setBackground(Color.yellow);
         
@@ -32,14 +32,14 @@ class SlidingPuzzle extends Frame implements ActionListener, KeyListener {
 
         c=0;
 
-        for(i=0;i<4;i++) {
-            for(j=0;j<4;j++) {
-                if(solutionArray[c]==0) {
-                    h=i;v=j;
-                    b[i][j]=new Button("");
+        for(i = 0; i < 4; i++) {
+            for(j = 0; j < 4; j++) {
+                if(solutionArray[c] == 0) {
+                    h = i; v = j;
+                    b[i][j] = new Button("");
                     b[i][j].setBackground(Color.white);
                 } else {
-                    b[i][j]=new Button(String.valueOf(solutionArray[c]));
+                    b[i][j] = new Button(String.valueOf(solutionArray[c]));
                     b[i][j].setBackground(Color.pink);
                 }
             c++;
@@ -48,15 +48,15 @@ class SlidingPuzzle extends Frame implements ActionListener, KeyListener {
 
         p.setLayout(new GridLayout(4,4));
 
-        for(i=0;i<4;i++) {
-            for(j=0;j<4;j++) {
+        for(i = 0; i < 4; i++) {
+            for(j = 0; j < 4; j++) {
                 b[i][j].setFont(f3);
                 p.add(b[i][j]);
             }
         }
 
-        for(i=0;i<4;i++) {
-            for(j=0;j<4;j++) {
+        for(i = 0; i < 4; i++) {
+            for(j = 0; j < 4; j++) {
                 b[i][j].addKeyListener(this);
                 add(l1, BorderLayout.NORTH);
                 add(p, BorderLayout.CENTER);
@@ -77,12 +77,12 @@ class SlidingPuzzle extends Frame implements ActionListener, KeyListener {
     }
 
     boolean checkWinner() {
-        int w=1;
+        int w = 1;
 
-        for(i=0;i<4;i++) {
-            for(j=0;j<4;j++) {
-                if(b[i][j].getLabel()!="") {
-                    if(Integer.parseInt(b[i][j].getLabel())!=w)
+        for(i = 0; i < 4; i++) {
+            for(j = 0; j < 4; j++) {
+                if(b[i][j].getLabel() != "") {
+                    if(Integer.parseInt(b[i][j].getLabel()) != w)
                         break;
                     else
                     w++;
@@ -90,10 +90,10 @@ class SlidingPuzzle extends Frame implements ActionListener, KeyListener {
             }
         }
 
-        if(w==15)
+        if (w == 15)
             return(true);
         else
-        return(false);
+            return(false);
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -101,8 +101,8 @@ class SlidingPuzzle extends Frame implements ActionListener, KeyListener {
     }
 
     public void keyPressed(KeyEvent e) {
-        if(e.getKeyCode( )==KeyEvent.VK_LEFT) {
-            if(v<3) {
+        if(e.getKeyCode( ) == KeyEvent.VK_LEFT) {
+            if(v < 3) {
                 b[h][v].setLabel(b[h][v+1].getLabel());
                 b[h][v+1].setLabel("");
                 b[h][v+1].setBackground(Color.white);
@@ -112,16 +112,16 @@ class SlidingPuzzle extends Frame implements ActionListener, KeyListener {
                 move++;
 
                 if(checkWinner()) {
-                    l2.setText("Congrats !!! Number of attempt= "+move);
+                    l2.setText("Congrats !!! Number of attempt= " + move);
                 } else {
-                    l2.setText(msg+" "+move);
+                    l2.setText(msg + " " + move);
                 }
             } else {
                 l2.setText("Invalid Move !!!");
             }
         }
 
-        if(e.getKeyCode( )==KeyEvent.VK_RIGHT) {
+        if(e.getKeyCode( ) == KeyEvent.VK_RIGHT) {
             if(v>0) {
                 b[h][v].setLabel(b[h][v-1].getLabel());
                 b[h][v-1].setLabel("");
@@ -140,7 +140,7 @@ class SlidingPuzzle extends Frame implements ActionListener, KeyListener {
             }
         }
 
-        if(e.getKeyCode( )==KeyEvent.VK_DOWN) {
+        if(e.getKeyCode( ) == KeyEvent.VK_DOWN) {
             if(h>0) {
                 b[h][v].setLabel(b[h-1][v].getLabel());
                 b[h-1][v].setLabel("");
@@ -150,9 +150,9 @@ class SlidingPuzzle extends Frame implements ActionListener, KeyListener {
                 move++;
 
                 if(checkWinner()) {
-                    l2.setText("Congrats !!! Number of attempt= "+move);
+                    l2.setText("Congrats !!! Number of attempt= " + move);
                 } else {
-                    l2.setText(msg+" "+move);
+                    l2.setText(msg + " " + move);
                 }
             } else {
                 l2.setText("Invalid Move !!!");
@@ -169,9 +169,9 @@ class SlidingPuzzle extends Frame implements ActionListener, KeyListener {
                 move++;
 
                 if(checkWinner()) {
-                    l2.setText("Congrats !!! Number of attempt= "+move);
+                    l2.setText("Congrats !!! Number of attempt= " + move);
                 } else {
-                    l2.setText(msg+" "+move);
+                    l2.setText(msg + " " + ove);
                 }
             } else {
                 l2.setText("Invalid Move !!!");
